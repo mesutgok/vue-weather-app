e56b1a60af93f11adb71140f26ac09db1498451a
Mesut GÖK <mgok@boynergrup.com>
2019-06-10 17:12:55 +0300

    sentry integrated

--- src/main.js
+++ src/main.js
@@ -5,9 +5,20 @@ Vue.config.productionTip = false
 
 //___________________________________________________________________________________________________ App Init And Config  __//
 
+
 import App from '@/App'
 import router from '@/router'
 import store from '@/store'
+
+//___________________________________________________________________________________________________ Sentry Config  __//
+
+import * as Sentry from '@sentry/browser';
+import * as Integrations from '@sentry/integrations';
+
+Sentry.init({
+  dsn: 'https://21eaa7a9e4e24ec8adcffae1578cebed@sentry.io/1478615',
+  integrations: [new Integrations.Vue({Vue, attachProps: true})],
+});
 
 /* Vue Instance Config */
 new Vue({
